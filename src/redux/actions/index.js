58c3eb0df8c88b87928c2088f5f704f94cba0ea9
baky2708 <flavioisaque123@gmail.com>
2022-01-14 17:03:56 -0300

    feat: 04 Requisito - Completo

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,3 +3,15 @@ export const SET_TOKEN = 'SET_TOKEN';
 export const setToken = (payload) => ({
   type: SET_TOKEN, payload,
 });
+
+export const SET_NAME = 'SET_NAME';
+
+export const setName = (payload) => ({
+  type: SET_NAME, payload,
+});
+
+export const SET_EMAIL = 'SET_EMAIL';
+
+export const setEmail = (payload) => ({
+  type: SET_EMAIL, payload,
+});
