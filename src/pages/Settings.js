61e8f619ca372923460a427878167b5404f20b93
baky2708 <flavioisaque123@gmail.com>
2022-01-14 15:46:55 -0300

    feat: 03 Requisito - Completo

---
+++ src/pages/Settings.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="settings-title">yayayayayayaya</p>
+      </div>
+    );
+  }
+}
+
+// const mapStateToProps = (state) => ({
+//   token: state.token.token,
+// });
+
+export default Settings;
